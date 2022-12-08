1954a9a02b2f83a5975bc6ea2f5ab9aeeeecc321
Atul <atulreso1@gmail.com>
2022-12-08 12:33:19 +0530

    Add:implement todo all

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import "./App.css";
 import EfficientSearch from "./EfficientSearch";
 import NaiveSearch2 from "./NaiveSearch2";
 import NaiveSerach1 from "./NaiveSerach1";
+import Todo from "./Todo";
 
 function App() {
   return (
     <div className="App">
-      <NaiveSerach1 />
+      {/* <NaiveSerach1 /> */}
       {/* <NaiveSearch2/> */}
       {/* <EfficientSearch/> */}
+      <Todo/>
     </div>
   );
 }
